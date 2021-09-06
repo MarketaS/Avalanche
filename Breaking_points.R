ef098a53b3ea56e94d5e3bebcd949c3b8eea52f4
Juras Roman <juras@fzp.czu.cz>
2021-09-06 13:14:24 +0200

    Separating the analysis for wet (C = 2) and slab (A = 2,3,4) avalanches. dta_melt is probably not needed to be updated. See modified data tables aval_total_lucb and aval_total_lbou

---
+++ Breaking_points.R
@@ -0,0 +1,10 @@
+# This scripts deals with the breaking points separating the cold and warm part of the snow season according to daily temperature
+# Created by @Roman.Juras
+
+
+# Testing several approaches
+# 1. Source: An overview of change point packages in R (https://lindeloev.github.io/mcp/articles/packages.html)
+
+# testing data set - Air temperature from Labska
+
+Season_temp <- Meteo_4Aval[]
